98276434b94ab47985724aa818e6abd40f12d834
AdityaK <amkhatry@gmail.com>
2024-02-23 10:00:23 +1100

    Adding a login form from the internet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { AuthComponent } from './auth/auth.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
+  {path: 'login', component: LoginComponent},
   { path: 'shopping-list', component: ShoppingListComponent },
   { path: 'auth', component: AuthComponent },
 ];
